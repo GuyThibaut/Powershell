5cd5944552738022e833113e33d2b3b8a634dd4d
GuyThibaut <thibaut.guy97@gmail.com>
2018-09-03 14:51:29 +0200

    calculette v0

---
+++ caclif.ps1
@@ -0,0 +1,22 @@
+﻿# Mise en place d'une calculatrice #
+
+echo " Pour utiliser cette calculatrice rentrer le premier nombre"
+
+[decimal]$numero1 = Read-Host " Premier nombre ?"
+
+echo "  
+        + Pour une addition
+        - Pour une soustraction
+        / Pour une division
+        * Pour une multiplication
+        
+        "
+
+$type = Read-Host "Quel type d'opération ?"
+
+[decimal]$numero2 = Read-Host " Deuxieme nombre ?"
+
+if($type -eq "+") { $numero1 + $numero2 }
+elseif($type -eq "-") { $numero1 - $numero2 }
+elseif($type -eq "/") { $numero1 / $numero2 }
+elseif($type -eq "*") { $numero1 * $numero2 }
---
+++ calc.ps1
@@ -0,0 +1,68 @@
+﻿# Mise en place d'une calculatrice #
+
+echo " Vous avez lancé une calculatrice 
+
+"
+do
+{
+[decimal]$numero1 = Read-Host " Premier nombre ?"
+echo ""
+
+echo "  
+        + Pour une addition
+        - Pour une soustraction
+        / Pour une division
+        * Pour une multiplication
+        
+        "
+
+$type = Read-Host "Quel type d'opération ?"
+echo ""
+echo ""
+
+
+[decimal]$numero2 = Read-Host " Deuxieme nombre ?"
+echo ""
+echo ""
+
+switch ($type)
+{
+    + 
+    {
+        $result = $numero1 + $numero2
+
+        echo " = $result "
+    }
+    -
+    {
+    $result = $numero1 - $numero2
+    
+    echo " = $result "
+    }
+    /
+    {
+        $result = $numero1 / $numero2
+
+        echo " = $result "
+    }
+    *
+    {
+    $result = $numero1 * $numero2
+
+    echo " = $result "
+    }
+
+    Default
+    {
+
+    echo "Opération Impossible"
+    
+    }
+}
+echo ""
+echo ""
+$exit = Read-Host "Quitter O/N"
+
+Clear-Host
+
+} While ($exit -like "N")
